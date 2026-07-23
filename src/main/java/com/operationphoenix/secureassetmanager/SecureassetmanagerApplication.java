8cd569b11331907fca701ba303b0e634dadeb2cd
Keamogetswe Mafora <keamogetswemafora@gmail.com>
2026-07-23 15:40:10 +0200

    Initial Sring boot project setup

---
+++ src/main/java/com/operationphoenix/secureassetmanager/SecureassetmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.operationphoenix.secureassetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureassetmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureassetmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/operationphoenix/secureassetmanager/SecureassetmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operationphoenix.secureassetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureassetmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
